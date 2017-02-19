345c33a4e97bc983acdf362910dcbf6c4327110b
ha5di <ha5di@outlook.com>
2017-02-19 12:53:34 +0100

    Bump version number

--- dixprscommon.py
+++ dixprscommon.py
@@ -3,14 +3,14 @@
 ####################################################
 # APRS digipeater and gateway for amateur radio use
 #
-# (C) HA5DI - 2012-2015
+# (C) HA5DI - 2012-2017
 #
 # http://sites.google.com/site/dixprs/
 ####################################################
 
-version = 'DIXPRS 2.4.0-beta3'
-versdat = '16-Sep-2015'
-dststrn = 'APDI24'
+version = 'DIXPRS 2.5.0'
+versdat = '19-Feb-2017'
+dststrn = 'APDI25'
 udpbase = 31110
 
 
